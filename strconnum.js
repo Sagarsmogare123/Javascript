d2bcb8147e62d59e1b79a615dd4e35b68b82c74a
sagar <59043146+Sagarsmogare123@users.noreply.github.com>
2022-10-17 16:56:53 +0530

    js files with solutions

---
+++ strconnum.js
@@ -0,0 +1,10 @@
+let str1 = "11";
+let num1 = 22;
+let sum1 = str1 + num1;
+console.log(sum1);
+
+let str2 = "33";
+let strCon = Number(str2);
+let num2 = 44;
+let sum2 = strCon + num2;
+console.log(sum2);
